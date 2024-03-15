9c8d478dd126dfdbd33eb8dfbe2671d2cbc2f514
Eric <mmmru7shj@gmail.com>
2024-03-15 17:23:54 +0100

    first commit

---
+++ bench.rb
@@ -0,0 +1,71 @@
+require 'erb'
+require 'haml'
+require 'slim'
+require 'benchmark'
+require 'ostruct'
+require "htmplt"
+
+notes = OpenStruct.new title: 'Write an essay', description: 'My essay is about...', randomList: (0..50).to_a.sort{ rand() - 0.5 }[0..10000]
+
+erb_example = <<-ERB_EXAMPLE
+<span><%= notes.title %></span>
+<span><%= notes.description %></span>
+<table>
+  <tr>
+    <% notes.randomList.each do |note| %>
+      <td><%= note %></td>
+    <% end %>
+  </tr>
+</table>
+ERB_EXAMPLE
+
+slim_example = <<-SLIM_EXAMPLE
+span= notes.title
+span= notes.description
+table
+  tr
+    - notes.randomList.each do |note|
+      td= note
+SLIM_EXAMPLE
+
+haml_example = <<-HAML_EXAMPLE
+%span= notes.title
+%span= notes.description
+%table
+  %tr
+    - notes.randomList.each do |note|
+      %td= note
+HAML_EXAMPLE
+
+class Notes < Component
+	def initialize(notes)
+		super()
+		@notes = notes
+	end
+
+	def render
+		span @notes.title
+		span @notes.description
+		table do 
+			tr do
+				@notes.randomList.each do |note|
+					td do
+						text note.to_s
+					end
+				end
+			end
+		end
+	end
+end
+
+context = OpenStruct.new notes: notes
+__result = ''
+
+Benchmark.bmbm(20) do |bcmk|
+  bcmk.report("erb_test") { (1..2000).each { ERB.new(erb_example, trim_mode: 0).result binding } }
+  bcmk.report("slim_test") { (1..2000).each{ Slim::Template.new { slim_example }.render(context) } }
+  bcmk.report("my_test") { (1..2000).each { 
+		Builder.run(Notes, context.notes)
+	} 
+  }
+end
---
+++ htmplt.gemspec
@@ -0,0 +1,9 @@
+Gem::Specification.new do |s|
+	s.name        = "htmplt"
+	s.version     = "0.0.0"
+	s.summary     = "HTML Builder using components"
+	s.description = "htmplt is a HTML-Template builder using ruby syntax."
+	s.authors     = ["unknown"]
+	s.files       = ["lib/htmplt.rb"]
+	s.license     = "MIT"
+end
